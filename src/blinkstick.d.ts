6f7e57ba26f12ee0096bb3428a77a934f6d5302b
Michael Scharl <michael.scharl@me.com>
2020-05-25 12:42:55 +0200

    Added types

--- src/blinkstick.d.ts
+++ src/blinkstick.d.ts
@@ -1,19 +1,28 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 declare module 'blinkstick' {
-    export interface MorphOptions {
+    export interface ColorOptions {
         channel?: number;
         index?: number;
+    }
+    export interface MorphOptions extends ColorOptions {
         duration?: number;
         steps?: number;
     }
 
     export interface BlinkStick {
         setMode(mode: 0 | 1 | 2): void;
+
         setInverse(inverse: boolean): void;
+
         getColor(callback: (error: any, red: number, green: number, blue: number) => void): void;
         getColor(index: number, callback: (error: any, red: number, green: number, blue: number) => void): void;
+
+        setColor(color: string, options?: ColorOptions, callback?: () => void): void;
+        setColor(red: number, green: number, blue: number, options?: ColorOptions, callback?: () => void): void;
+
         morph(color: string, options?: MorphOptions, callback?: () => void): void;
         morph(red: number, green: number, blue: number, options?: MorphOptions, callback?: () => void): void;
+
         turnOff(): void;
     }
 
